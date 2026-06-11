442ae07020ed2666f9ac3437d8aad08fa40828c6
DESKTOP-7MRLFU1\seeroo <junho.kim@seeroo.co.kr>
2026-06-11 18:01:44 +0900

    fix: telephony plugin namespace issue in build.gradle.kts

--- card_siljeok/android/build.gradle.kts
+++ card_siljeok/android/build.gradle.kts
@@ -16,6 +16,19 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    afterEvaluate {
+        if (hasProperty("android")) {
+            try {
+                val androidExt = extensions.getByName("android")
+                val namespaceProp = androidExt.javaClass.getMethod("getNamespace").invoke(androidExt)
+                if (namespaceProp == null) {
+                    val groupStr = project.group.toString()
+                    val validNamespace = if (groupStr.isNotEmpty()) groupStr else "com.plugin.${project.name.replace("-", "_")}"
+                    androidExt.javaClass.getMethod("setNamespace", String::class.java).invoke(androidExt, validNamespace)
+                }
+            } catch (e: Exception) {}
+        }
+    }
     project.evaluationDependsOn(":app")
 }
 
